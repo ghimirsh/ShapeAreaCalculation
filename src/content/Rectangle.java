87846b06ad7cf331d3ac8e99cd7d6e336b722460
Shankar Ghimire <ghimirsh@sheridancollege.ca>
2020-02-16 20:14:21 -0500

    Adds Rectangle.java class in the project.

---
+++ src/content/Rectangle.java
@@ -0,0 +1,46 @@
+/*
+ * Shankar Ghimire
+ * StudentId:991585190
+ * Course: ...............
+ */
+package content;
+
+import content.*;
+
+/**
+ *
+ * @author Dell
+ */
+public class Rectangle {
+    private double length;
+    private double width;
+
+    /**
+     * @return the length
+     */
+    public double getLength() {
+        return length;
+    }
+
+    /**
+     * @param length the length to set
+     */
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    /**
+     * @return the width
+     */
+    public double getWidth() {
+        return width;
+    }
+
+    /**
+     * @param width the width to set
+     */
+    public void setWidth(double width) {
+        this.width = width;
+    }
+    
+}
